8208d26d936dda4e05fa0e70aa4deea0d128ddfb
Joshua Sierles <joshua@diluvia.net>
2016-10-19 13:12:00 +0200

    allow passing volume options via the environment. for things like --enable-root-creation fix check for infinit daemon mode

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
-infinit-user --import -i /infinit/user.json
+infinit-user --import -i /etc/infinit/user.json
 infinit-network --fetch --as $USER
 infinit-volume --fetch --as $USER
 
-if [ $MODE -eq "server" ]; then
+if [ "$MODE" = "server" ]; then
   if infinit-storage --list | grep local; then
     echo "Using local storage"
   else
-    echo "Creating infinit storage area /infinit_storage"
-    infinit-storage --create --filesystem --name local --path /infinit_storage
+    echo "Creating infinit storage"
+    infinit-storage --create --filesystem --name local
   fi
   NETWORK_OPTIONS="--storage local"
 fi
@@ -16,4 +16,4 @@ if infinit-network --list --as $USER | grep $NETWORK | grep "not linked"; then
   infinit-network --link --as $USER --name $NETWORK $NETWORK_OPTIONS
 fi
 
-infinit-volume --mount --as $USER --name $VOLUME $OPTIONS --publish --mountpoint /infinit_mount
+infinit-volume --mount --as $USER --name $VOLUME $VOLUME_OPTIONS --publish --mountpoint /infinit_mount
